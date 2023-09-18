273f12fd7dbebf59d2f139ca771e391dc7f2b5bc
Sam Tran <samuelttran@gmail.com>
2023-09-18 09:19:50 -0600

    Correct to have #price display as Float

--- spec/item.spec.rb
+++ spec/item.spec.rb
@@ -5,17 +5,19 @@ RSpec.describe Item do
     @item1 = Item.new({name: 'Peach', price: '$0.75'})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
+
   describe '#initialize' do
     it 'exists, has an instance' do
       expect(@item1).to be_instance_of(Item)
+      expect(@item2).to be_instance_of(Item)
     end
 
     it 'has attributes upon initilize' do
       expect(@item1.name).to eq('Peach')
       expect(@item2.name).to eq('Tomato')
 
-      expect(@item1.price).to eq('$0.75')
-      expect(@item2.price).to eq('$0.50')
+      expect(@item1.price).to eq(0.75)
+      expect(@item2.price).to eq(0.5)
     end
   end
 end
