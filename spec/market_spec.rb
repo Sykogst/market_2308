0c0fc79be0e63bac4cde721c6f6eb166753cf6a0
Sam Tran <samuelttran@gmail.com>
2023-09-18 09:56:24 -0600

    refactor Item class to convert price from string so it is easily used as a Float later

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -5,6 +5,15 @@ require './lib/market'
 RSpec.describe Market do
   before(:each) do
     @market1 = Market.new('Belmar Market')
+
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")  
+
+    @item1 = Item.new({name: 'Peach', price: '$0.75'})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   describe '#initialize' do
@@ -20,13 +29,13 @@ RSpec.describe Market do
 
   describe '#other stuff' do
   # before(:each) do
-  #   @vendor1 = Vendor.new('Rocky Mountain Fresh')
-  #   @item1 = Item.new({name: 'Peach', price: '$0.75'})
-  #   @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor1 = Vendor.new('Rocky Mountain Fresh')
+    @item1 = Item.new({name: 'Peach', price: '$0.75'})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
 
-  #   @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
 
-  #   @vendor3 = Vendor.new("Palisade Peach Shack")  
-  # end
+    @vendor3 = Vendor.new("Palisade Peach Shack")  
+  end
   end
 end
