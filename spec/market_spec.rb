ed96e7949739a419358c3d116854e3811ed69f71
Sam Tran <samuelttran@gmail.com>
2023-09-18 10:01:24 -0600

    Write test for #add_vendor

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -14,6 +14,13 @@ RSpec.describe Market do
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    # Stock items into vendors (might move later)
+    @vendor1.stock(@item1, 35) 
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50) 
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65) 
   end
 
   describe '#initialize' do
@@ -27,15 +34,16 @@ RSpec.describe Market do
     end
   end
 
-  describe '#other stuff' do
-  # before(:each) do
-    @vendor1 = Vendor.new('Rocky Mountain Fresh')
-    @item1 = Item.new({name: 'Peach', price: '$0.75'})
-    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  describe '#add_vendor, #vendor_names' do
+    it 'Adds vendor name to market vendors' do
+      expect(@market1.vendors).to eq([])
+      
+      @market.add_vendor(@vendor1)
+      expect(@market1.vendors).to eq([@vendor1])
 
-    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
-
-    @vendor3 = Vendor.new("Palisade Peach Shack")  
-  end
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market1.vendors).to eq([@vendor1, @vendor2, @vendor3])
+    end
   end
 end
